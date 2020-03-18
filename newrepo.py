4874ed85419ff243792c7d8559b384b284603dc6
brendan nyambati <brendan.nyambati@gmail.com>
2020-03-18 13:58:46 -0500

    created project folder where everything goes down

--- newrepo.py
+++ newrepo.py
@@ -4,12 +4,15 @@ print("--------------------------------------")
 user_name = input("User: ") # github user name
 repo_name = input("Repository: ") # github repository
 
+# Create the project folder
+create_repo_string = "mkdir {}&&".format(repo_name)
+create_repo_string += "cd {}&&touch x&&".format(repo_name)
+
 # Using github's api to create (repo_name) at (user_name)'s profile
-create_repo_string = """curl -u '{}' https://api.github.com/user/repos -d '{{"name":"{}"}}'""".format(user_name, repo_name)
+create_repo_string += """curl -u '{}' https://api.github.com/user/repos -d '{{"name":"{}"}}' > x""".format(user_name, repo_name, repo_name)
 
 # set_up_repo is one long string with a bunch of commands tied with && to sync local machine to github
-set_up_repo = "mkdir {}&&".format(repo_name)
-set_up_repo += "cd {}&&".format(repo_name)
+set_up_repo = "cd {}&&rm x&&".format(repo_name)
 set_up_repo += """echo "## {} ##" > README.md&&""".format(repo_name) # Create README
 set_up_repo += "git init&&" # Initialize git
 set_up_repo += "git add .&&" # Add all files in current directory
@@ -18,8 +21,7 @@ set_up_repo += "git remote add origin git@github.com:{}/{}.git&&".format(user_na
 set_up_repo += "git push -u origin master" # Pushing everything into master
 
 # Excecuting (create_repo_string) & (set_up_repo) commands below
-os.system(create_repo_string + " > {}/x".format(repo_name)) # create_repo_string's output is displayed into a file named x
-os.system("rm ./{}/x".format(repo_name)) # The file named x is useless imo so get i'm getting rid of it
-os.system(set_up_repo) 
+os.system(create_repo_string)
+os.system(set_up_repo)
 
-print("\n\nCreated: " + repo_name + " @ " + user_name + "\n\n")
+print("\n\n:::::::Created: " + repo_name + " @ " + user_name + "\n\n")
